0270065fbd4ea9b37c6d9b48dd707ac6f727eea5
vopl <vopl@bk.ru>
2010-11-14 00:00:00 +0300

    .

--- scripts/plugins/forum/point/preFilter.js
+++ scripts/plugins/forum/point/preFilter.js
@@ -3,7 +3,30 @@ if(!ui.blocks.breadcrumbs)
 	ui.blocks.breadcrumbs = this.parent.properties.breadcrumbs();
 }
 
-if(!ui.blocks.left)
+if(!ui.blocks.header)
 {
-	ui.blocks.left = router.getPoint('/user/authorize').render();
+	let t = arguments.callee.header;
+	if(!t)
+	{
+		t = router.createTemplate();
+		
+		t.compile(<>
+			<table>
+				<tr>
+					<td>
+						<img src='/dog.png'/>
+					</td>
+					<td width='100%'>
+					</td>
+					<td align='right'>
+						{t.auth}
+					</td>
+				</tr>
+			</table>
+		</>);
+		arguments.callee.header = t;
+	}
+	t = t.clone();
+	t.auth = router.getPoint('/user/authorize').render();
+	ui.blocks.header = t;
 }
