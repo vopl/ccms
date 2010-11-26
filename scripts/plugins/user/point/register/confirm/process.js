fa9434432066190ac715c9ba44270f0d7384a32a
vopl <vopl@bk.ru>
2010-11-27 00:00:00 +0300

    .

--- scripts/plugins/user/point/register/confirm/process.js
+++ scripts/plugins/user/point/register/confirm/process.js
@@ -5,13 +5,15 @@ if(request.params.secret)
 	if(user)
 	{
 		orm.exec("UPDATE {User} SET status='ok' WHERE id=$1", user.id);
-		ui.blocks.center.push(this.properties.renderOk());
 
 		router.cd.session.userId = user.id;
 		global.user = user;
 
 		router.cd.global.session.remember = false;
 		request.pushHeader('Set-Cookie', 'sid='+router.cd.global.session.id+'; path=/');
+
+		ui.blocks.center.push(this.properties.renderOk());
+
 
 	}
 	else
