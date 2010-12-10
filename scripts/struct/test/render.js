3e8cd822e3be46505d481559f6db9e2e6cc80845
vopl <vopl@bk.ru>
2010-12-11 00:00:00 +0300

    .

--- scripts/struct/test/render.js
+++ scripts/struct/test/render.js
@@ -1,4 +1,17 @@
-﻿let teman = router.plugins.texteditor.manager;
+﻿function f()
+{
+	let data = {a:'строка', b:[], c:0, d:{}};
+	let pswd = crypto.rand.str_(8);
+	let code1 = crypto.sym.aes.encodeJson(pswd, data);
+	let code2 = crypto.sym.des.encodeJson(pswd, data);
 
-let te = teman.getTe(this.path, {engine:'tmce', option2:'value2'});
-return te.render();
+	let res1 = crypto.sym.aes.decodeJson(pswd, code1);
+	let res2 = crypto.sym.des.decodeJson(pswd, code2);
+
+	return dumps([data, pswd, code1, code2, res1, res2]);
+}
+
+//let teman = router.plugins.texteditor.manager;
+
+//let te = teman.getTe(this.path, {engine:'tmce', option2:'value2'});
+return <pre>{f.toString()}{f()}</pre>;//+te.render();
